7c833ce01064f827cf44161d936d0fd963e0537e
VictorIsaias <isaias.castrejonrdz@gmail.com>
2024-04-17 11:14:09 -0600

    funciones

--- Lista.js
+++ Lista.js
@@ -1,3 +1,33 @@
 class lista{
+    lista() {
+        this.p=new Nodo()
+        this.f=new Nodo()
+    }
+
+    mostrar(){
+
+    }
+
+    insertar(){
+
+    }
+
+    eliminar(){
+
+    }
+
+    buscar(){
+
+    }
+
+    reiniciar(){
+
+    }
+
+    modificar(){
+
+    }
+    
+
     
 }
--- Nodo.js
+++ Nodo.js
@@ -1,3 +1,27 @@
 class nodo{
+    nodo(){
+        this.dato=null
+        this.sig=this.ant=null
+    }
 
+    getDato(){
+        return dato
+    }
+    setDato(dato){
+        this.dato=dato
+    }
+
+    getSig(){
+        return dato
+    }
+    setSig(sig){
+        this.sig=sig
+    }
+
+    getAnt(){
+        return ant
+    }
+    setDato(ant){
+        this.ant=ant
+    }
 }
